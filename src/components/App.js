569f1dbfa2507780386b0630204f9abebaa16b17
Johnny1992 <evgeny.frontend@gmail.com>
2020-08-27 22:52:19 +0300

    added redux and first store

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { createStore } from "redux";
 
 const App = () => {
   return <div>hello</div>;
 };
 
 export default App;
+
+const hello = () => ({ welcome: "hello" });
+const store = createStore(hello);
+console.log("store", store.getState());

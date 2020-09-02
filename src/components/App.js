bc6411708b5d28c75cb311384269b5790dde443d
Johnny1992 <evgeny.frontend@gmail.com>
2020-09-02 20:54:16 +0300

    added reducer and action

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,27 @@ const App = () => {
 
 export default App;
 
-const hello = () => ({ welcome: "hello" });
-const store = createStore(hello);
+const defaulState = {
+  welcome: "Hi",
+  otherState: "some stuff",
+};
+
+const greeting = (state = defaulState, action) => {
+  switch (action.type) {
+    case "GREET_ME":
+      return { ...state, welcome: "Hello Johnny" };
+    case "GREET_WORLD":
+      return { welcome: "Hello World!" };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(greeting);
+console.log("store", store.getState());
+
+store.dispatch({
+  type: "GREET_ME",
+});
+
 console.log("store", store.getState());

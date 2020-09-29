d80b3dfcb4d31e048fda06aab300e3d6bcae1587
Евгений Минин <evgeny.frontend@gmail.com>
2020-09-29 20:19:04 +0300

    action type constants

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,15 @@
+import { TOGGLE_MESSAGE } from "./actions";
+
 const initialState = {
-  messageVisibility: false
+  messageVisibility: false,
 };
 
 export default function (state = initialState, action) {
-  const {type} = action;
+  const { type } = action;
 
   switch (type) {
-    case 'TOGGLE_MESSAGE':
-      return {...state, messageVisibility: !state.messageVisibility};
+    case TOGGLE_MESSAGE:
+      return { ...state, messageVisibility: !state.messageVisibility };
     default:
       return state;
   }
